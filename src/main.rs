c143af81efe8d063014abda5817eff618c520704
SomeGuy13 <isaac7erickson@gmail.com>
2024-10-26 21:12:47 -0600

    first basic image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    
+    // Image
+
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    // Render
+
+    // This sets up format information for the .ppm file.
+    println!("P3\n {} {} \n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+
+    // This puts the image out to the .ppm file.
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b: f64 = 0.0;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            println!("{} {} {}\n", ir, ig, ib);
+        }
+    }
 }
